39a844d7e77eda27d2174161de3d6c5d5e301876
Harrison Sabean <hsabes@gmail.com>
2022-05-20 17:02:12 -0700

    commit tree 4bc5f7a4ee9e18e9cbc4f716dd357dc244ae9162

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let array = ['name1', 'name2', 'name3'];
+
+function writeCards(array, event){
+    for (let i = 0; i < 3; i++){
+        array[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }
+    return array;
+}
+
+function countDown(){
+    for (let i = 10; i >= 0; i--){
+        console.log(i);
+    }
+}
+
+countDown();
